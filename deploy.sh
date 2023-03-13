8b0ce1315af05845100326f16fbf687b8375ea53
FlameHorizon <shobun14@gmail.com>
2023-03-13 09:16:00 +0100

    Added aliases

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 sudo cp i3status.conf /etc/i3status.conf
+sudo cp .bashrc ~/.bashrc
 
 echo "Changes deployed."
--- pull-setup.sh
+++ pull-setup.sh
@@ -1,4 +1,5 @@
 # Copy configuration file for i3status bar
 cp /etc/i3status.conf i3status.conf
+sudo cp ~/.bashrc .bashrc
 
 echo "Coping completed"
