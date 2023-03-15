44331ce1126e017e3877b8f8c9eae5de56014971
FlameHorizon <shobun14@gmail.com>
2023-03-15 20:22:57 +0100

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 sudo cp i3status.conf /etc/i3status.conf
+sudo cp config ~/.config/i3/config
 
 sudo cp .bashrc ~/.bashrc
 source  ~/.bashrc
--- pull-setup.sh
+++ pull-setup.sh
@@ -1,5 +1,9 @@
 # Copy configuration file for i3status bar
 cp /etc/i3status.conf i3status.conf
+
+# Pull main i3 conf. file
+cp ~/.config/i3/config config
+
 sudo cp ~/.bashrc .bashrc
 
 echo "Coping completed"
